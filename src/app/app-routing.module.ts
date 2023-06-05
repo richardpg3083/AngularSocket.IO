9e069fa4c54d1b63c84eab585cc3206a40157d74
Richard3083 <116691720+Richard3083@users.noreply.github.com>
2023-06-04 21:42:08 -0400

    chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
@@ -6,11 +7,12 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path:'', component: HomeComponent},
-  {path:'chat/:user/:avatar',component:ChatComponent}
+  {path:"chat/:user/:avatar",component:ChatComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Params } from '@angular/router';
+import {param } from 'jquery';
+import * as $ from 'jquery';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+    public nickName:string='';
+    public avatar:string='';
+    
+ constructor(private rutaActiva: ActivatedRoute){
 
+ }
+ ngOnInit(): void {
+  const user=this.rutaActiva.snapshot.paramMap.get('user');
+  if(user)
+  {
+      this.nickName+=`${user}`;
+  }
+  const avat=this.rutaActiva.snapshot.paramMap.get('avatar');
+  if(avat)
+  {
+      this.avatar+=`${avat}`;
+  }
+    $("#userChat").html(this.nickName+"<br /><p class='text-dark fs-6'>en línea</p>");
+    $("#UserNick").val(this.nickName);
+  $("#imgUserchat").attr("src", "../../../assets/img/"+this.avatar+".png");
+ }
 }
